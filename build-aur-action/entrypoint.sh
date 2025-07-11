c63a650b6c97f1cb4c6f92be32ad7d7b9dd977aa
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2025-07-11 15:57:08 +0800

    Update entrypoint.sh

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -20,6 +20,10 @@ if [ ! -z "$INPUT_PREINSTALLPKGS" ]; then
     pacman -Syu --noconfirm "$INPUT_PREINSTALLPKGS"
 fi
 
-sudo --set-home -u builder yay -S --noconfirm --builddir=./ "$pkgname"
+sudo --set-home -u builder bash -c '
+  git clone "https://aur.archlinux.org/${pkgname}.git"
+  cd "${pkgname}"
+  makepkg -s
+'
 cd "./$pkgname" || exit 1
 python3 ../build-aur-action/encode_name.py
